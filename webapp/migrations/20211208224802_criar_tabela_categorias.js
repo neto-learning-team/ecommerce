23051138455cc7cf3a486dc893f6bb5acaa57123
lucas <ljeronimodarocha@gmail.com>
2021-12-11 09:26:22 -0300

    criação da tabela produtos e categorias.

---
+++ webapp/migrations/20211208224802_criar_tabela_categorias.js
@@ -0,0 +1,15 @@
+
+exports.up = async function (knex) {
+    knex.schema.hasTable('categorias').then(async function (exists) {
+        if (!exists) {
+            await knex.schema.createTable('categorias', function (table) {
+                table.increments().notNullable();
+                table.text("descricao").notNullable();
+            });
+        };
+    })
+}
+
+exports.down = async function (knex) {
+    knex.schema.dropTableIfExists('categorias').dropTableIfExists('produtos');
+};
