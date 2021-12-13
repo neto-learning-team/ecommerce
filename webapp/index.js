9cf20264626765779589470da962e186d3507f3f
lucas <ljeronimodarocha@gmail.com>
2021-12-13 01:05:44 -0300

    component header finished.

--- webapp/index.js
+++ webapp/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const routes = require('./api/routes')
 
+console.log("teste");
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+routes(app);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://dev.localhost:${port}`)
 })
+module.exports = app;
