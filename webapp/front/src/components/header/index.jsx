82c68bf2fab986ac6a470cee546e333595aa686e
Lucas Jeronimo da Rocha <ljeronimodarocha@gmail.com>
2021-12-28 18:57:50 -0300

    .

--- webapp/front/src/components/header/index.jsx
+++ webapp/front/src/components/header/index.jsx
@@ -1,5 +1,5 @@
 const React = require('react');
-const { FontAwesomeIcon } = require( '@fortawesome/react-fontawesome');
+const { FontAwesomeIcon } = require('@fortawesome/react-fontawesome');
 const { faPhoneAlt, faEnvelope } = require('@fortawesome/free-solid-svg-icons');
 const { faFacebookF, faTwitter, faInstagram, faLinkedinIn, faBehance } = require('@fortawesome/free-brands-svg-icons');
 
@@ -8,20 +8,20 @@ export default function Header(props) {
 
     return <header >
         <div className='header'>
-            <div className='contact'>
-                <FontAwesomeIcon icon={faPhoneAlt} />
-                <h3>+1 123 456 789</h3>
-                <div className='pipe'></div>
-                <FontAwesomeIcon icon={faEnvelope} />
-                <h3>info@company.com</h3>
-            </div>
-            <div className='social-media'>
-                <FontAwesomeIcon icon={faFacebookF} />
-                <FontAwesomeIcon icon={faTwitter} />
-                <FontAwesomeIcon icon={faInstagram} />
-                <FontAwesomeIcon icon={faLinkedinIn} />
-                <FontAwesomeIcon icon={faBehance} />
-            </div>
+            <ul className='contact'>
+                <li><FontAwesomeIcon icon={faPhoneAlt} /></li>
+                <li><h3>+1 123 456 789</h3></li>
+                <li className='pipe'></li>
+                <li><FontAwesomeIcon icon={faEnvelope} /></li>
+                <li> <h3>info@company.com</h3></li>
+            </ul>
+            <ul className='social-media'>
+                <li><FontAwesomeIcon icon={faFacebookF} /></li>
+                <li><FontAwesomeIcon icon={faTwitter} /></li>
+                <li><FontAwesomeIcon icon={faInstagram} /></li>
+                <li><FontAwesomeIcon icon={faLinkedinIn} /></li>
+                <li><FontAwesomeIcon icon={faBehance} /></li>
+            </ul>
         </div>
     </header>
 }
